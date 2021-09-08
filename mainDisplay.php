d60eb99a207cc073a9a36d0ad6f13536de430877
Mavs <marven.francisco@unc.edu.ph>
2021-09-08 18:20:32 +0800

    update

--- mainDisplay.php
+++ mainDisplay.php
@@ -1,3 +1,14 @@
-<?php
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Punica Inventory Management</title>
+</head>
+<body>
+    <div class="container">
+    <button class="btn btn-primary my-5">Add Product</button>
+    </div>
+</body>
+</html>
